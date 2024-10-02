2d488fdf833a901d0caa4fd023470771367d92ff
Camilobarre <barrecami@gmail.com>
2024-10-02 08:09:27 -0500

    feat: Define the HTML route online in the Welcome Page

--- Program.cs
+++ Program.cs
@@ -20,6 +20,39 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+// Define the HTML route online
+app.MapGet("/", () => Results.Content(@"
+    <!DOCTYPE html>
+    <html lang='es'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>TechStore</title>
+        <style>
+            body {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                height: 100vh;
+                margin: 0;
+                text-align: center;
+            }
+            h1 {
+                border: red solid;
+            }
+            p {
+                border: blue solid;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Welcome to TechStore API</h1>
+        <p>Consult the documentation in <a href='/swagger/index.html'>Swagger</a>.</p>
+    </body>
+    </html>
+", "text/html"));
+
 app.MapControllers();
 
 app.Run();
